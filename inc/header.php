3804d67d06a199c143b85afaf634664618e62012
Cynthia Brown <titaniaann@gmail.com>
2018-02-15 13:38:49 -0600

    Initial commit

---
+++ inc/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+  	<meta name="description" content="Author: Cynthia Brown, Category: Games">
+  	<meta name="keywords" content="">
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?php echo $title ?></title>
+	
+</head>
+<body>
+
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+$title = 'Games';
+include_once('inc/header.php');
+include_once('inc/nav.php');
+
+include_once('inc/footer.php');
+?>
