7f3809b3bfb852f6ff112d8b7bec67183ebd41f1
Cynthia Brown <titaniaann@gmail.com>
2018-02-20 07:44:21 -0600

    update

--- inc/header.php
+++ inc/header.php
@@ -4,8 +4,19 @@
 	<meta charset="UTF-8">
   	<meta name="description" content="Author: Cynthia Brown, Category: Games">
   	<meta name="keywords" content="">
-  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title><?php echo $title ?></title>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/
+bootstrap.min.css">
+
+	
+	<!-- jQuery first, then Tether, then Bootstrap JS. -->
+ 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+ 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.
+js"></script>
+ 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></
+script>
 	
 </head>
 <body>
